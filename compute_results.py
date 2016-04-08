89f8cac16ae6cf03d73526f6ab6a5afed45d4011
Steven Schmidt <schmidticus@gmail.com>
2016-04-07 21:33:43 -0600

    Updated 'compute_results.py' so it handles multiple base-files if necessary

--- compute_results.py
+++ compute_results.py
@@ -17,7 +17,7 @@ def test_permutation(id_names_files, input_file):
     timestamps = inputdata[:,1]
 
     kdidentifier = KDIdentifier(id_names_files,
-                                history_length=100,
+                                history_length=200,
                                 use_log_norm_pdf=True)
                                 
     tyoracidentifier = typeroracle.build_typeroracle(id_names_files)
@@ -41,30 +41,34 @@ id_names = ["joseph", "steven", "nozomu", "wilson", "lawrence", "jeff"]
 file_base_names = ["gettysburg"]
 file_postfixes = [".txt","2.txt","3.txt"]
 
-for leaveoutindex in range(0,3):
 
-    # Construct the training data input, and test input file list
-    id_names_files = dict()
-    input_files = dict()
-    for id in id_names:
-        files_for_id = []
-        input_file = ""
-        for file_base in file_base_names:
+for file_base in file_base_names:
+    print("File: ",file_base)
+    for leaveoutindex in range(0,3):
+    
+        print("Input index used for testing: ",leaveoutindex, " (others used for training)")
+    
+        # Construct the training data input, and test input file list
+        id_names_files = dict()
+        input_files = dict()
+        for id in id_names:
+            files_for_id = []
+            input_file = ""
             for i,postfix in enumerate(file_postfixes):
                 filename = "data/"+id+"_"+file_base+postfix
                 if(i != leaveoutindex):   
                     files_for_id.append(filename)
                 else:
                     input_file = filename
-        id_names_files[id] = files_for_id
-        input_files[id] = input_file
+            id_names_files[id] = files_for_id
+            input_files[id] = input_file
 
-    print("Input index used for testing: ",leaveoutindex, " (others used for training)")
+        # Now we do the testing
+        for id in id_names:
 
-    # Test on each test input.
-    for id in id_names:
-        guess = test_permutation(id_names_files,input_files[id])
-        print("Guess for '"+id+"': "+str(guess))
+            # Test on each test input.
+            guess = test_permutation(id_names_files,input_files[id])
+            print("Guess for '"+id+"': "+str(guess))
 
     
 
